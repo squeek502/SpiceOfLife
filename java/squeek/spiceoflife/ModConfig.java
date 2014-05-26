7354150cdf9167ab8fa7e83fc36479694a9125d3
squeek <squeek502@hotmail.com>
2014-05-25 23:50:36 -0700

    Config documentation + plural/singular tooltip string fix

--- java/squeek/spiceoflife/ModConfig.java
+++ java/squeek/spiceoflife/ModConfig.java
@@ -14,18 +14,34 @@ public class ModConfig
 	public static String FOOD_MODIFIER_FORMULA = ModConfig.FOOD_MODIFIER_FORMULA_STRING_DEFAULT;
 	private static final String FOOD_MODIFIER_FORMULA_STRING_NAME = "food.modifier.formula";
 	private static final String FOOD_MODIFIER_FORMULA_STRING_DEFAULT = "MAX(0, (1 - count/12))^MAX(0, food_hunger_value-ROUND(MAX(0, 1 - count/12), 0))";
-
+	private static final String FOOD_MODIFIER_FORMULA_STRING_COMMENT = 
+			"Uses the EvalEx expression parser\n"
+			+ "See: https://github.com/uklimaschewski/EvalEx for syntax/function documentation\n\n"
+			+ "Available variables:\n"
+			+ "\tcount : The number of times the food has been eaten (out of the last max_history_length foods)\n"
+			+ "\tmax_history_length : The maximum number of foods that are stored in the history at a time\n"
+			+ "\tcur_history_length : The current number of foods that are stored in the history (<= max_history_length)\n"
+			+ "\tfood_hunger_value : The default amount of hunger the food would restore (1 hunger unit = 1/2 hunger bar)\n"
+			+ "\tfood_saturation_mod : The default saturation modifier of the food\n"
+			+ "\tcur_hunger : The current hunger value of the player (20 = full)\n"
+			+ "\tcur_saturation : The current saturation value of the player\n"
+			+ "\ttotal_food_eaten : The all-time total number of food ever eaten by the player\n"
+			;
+	
 	public static int FOOD_HISTORY_LENGTH = ModConfig.FOOD_HISTORY_LENGTH_DEFAULT;
 	private static final String FOOD_HISTORY_LENGTH_NAME = "food.history.length";
 	private static final int FOOD_HISTORY_LENGTH_DEFAULT = 12;
+	private static final String FOOD_HISTORY_LENGTH_COMMENT = "The maximum amount of eaten foods stored in the history at a time";
 
 	public static boolean FOOD_HISTORY_PERSISTS_THROUGH_DEATH = ModConfig.FOOD_HISTORY_PERSISTS_THROUGH_DEATH_DEFAULT;
 	private static final String FOOD_HISTORY_PERSISTS_THROUGH_DEATH_NAME = "food.history.persists.through.death";
-	private static final boolean FOOD_HISTORY_PERSISTS_THROUGH_DEATH_DEFAULT = true;
+	private static final boolean FOOD_HISTORY_PERSISTS_THROUGH_DEATH_DEFAULT = false;
+	private static final String FOOD_HISTORY_PERSISTS_THROUGH_DEATH_COMMENT = "If true, food history will not get reset after every death";
 
 	public static int FOOD_EATEN_THRESHOLD = ModConfig.FOOD_EATEN_THRESHOLD_DEFAULT;
 	private static final String FOOD_EATEN_THRESHOLD_NAME = "new.player.food.eaten.threshold";
 	private static final int FOOD_EATEN_THRESHOLD_DEFAULT = ModConfig.FOOD_HISTORY_LENGTH / 2;
+	private static final String FOOD_EATEN_THRESHOLD_COMMENT = "The number of times a new player (by World) needs to eat before this mod has any effect";
 	
 
 	private static Configuration config;
@@ -36,10 +52,10 @@ public class ModConfig
 
 		load();
 
-		FOOD_MODIFIER_FORMULA = config.get(CATEGORY_MAIN, FOOD_MODIFIER_FORMULA_STRING_NAME, FOOD_MODIFIER_FORMULA_STRING_DEFAULT).getString();
-		FOOD_HISTORY_LENGTH = config.get(CATEGORY_MAIN, FOOD_HISTORY_LENGTH_NAME, FOOD_HISTORY_LENGTH_DEFAULT).getInt();
-		FOOD_HISTORY_PERSISTS_THROUGH_DEATH = config.get(CATEGORY_MAIN, FOOD_HISTORY_PERSISTS_THROUGH_DEATH_NAME, FOOD_HISTORY_PERSISTS_THROUGH_DEATH_DEFAULT).getBoolean(FOOD_HISTORY_PERSISTS_THROUGH_DEATH_DEFAULT);
-		FOOD_EATEN_THRESHOLD = config.get(CATEGORY_MAIN, FOOD_EATEN_THRESHOLD_NAME, FOOD_EATEN_THRESHOLD_DEFAULT).getInt();
+		FOOD_MODIFIER_FORMULA = config.get(CATEGORY_MAIN, FOOD_MODIFIER_FORMULA_STRING_NAME, FOOD_MODIFIER_FORMULA_STRING_DEFAULT, FOOD_MODIFIER_FORMULA_STRING_COMMENT).getString();
+		FOOD_HISTORY_LENGTH = config.get(CATEGORY_MAIN, FOOD_HISTORY_LENGTH_NAME, FOOD_HISTORY_LENGTH_DEFAULT, FOOD_HISTORY_LENGTH_COMMENT).getInt();
+		FOOD_HISTORY_PERSISTS_THROUGH_DEATH = config.get(CATEGORY_MAIN, FOOD_HISTORY_PERSISTS_THROUGH_DEATH_NAME, FOOD_HISTORY_PERSISTS_THROUGH_DEATH_DEFAULT, FOOD_HISTORY_PERSISTS_THROUGH_DEATH_COMMENT).getBoolean(FOOD_HISTORY_PERSISTS_THROUGH_DEATH_DEFAULT);
+		FOOD_EATEN_THRESHOLD = config.get(CATEGORY_MAIN, FOOD_EATEN_THRESHOLD_NAME, FOOD_EATEN_THRESHOLD_DEFAULT, FOOD_EATEN_THRESHOLD_COMMENT).getInt();
 
 		save();
 	}
