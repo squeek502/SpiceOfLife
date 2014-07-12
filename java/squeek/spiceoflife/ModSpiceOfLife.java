f33e73803780d749656656384c77934246ff60c8
squeek <squeek502@hotmail.com>
2014-07-11 22:42:11 -0700

    Add support for the Version Checker mod

--- java/squeek/spiceoflife/ModSpiceOfLife.java
+++ java/squeek/spiceoflife/ModSpiceOfLife.java
@@ -14,6 +14,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
@@ -58,6 +59,7 @@ public class ModSpiceOfLife
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
+		FMLInterModComms.sendRuntimeMessage(ModInfo.MODID, "VersionChecker", "addVersionCheck", "http://www.ryanliptak.com/minecraft/versionchecker/squeek502/SpiceOfLife");
 	}
 
 	@EventHandler
