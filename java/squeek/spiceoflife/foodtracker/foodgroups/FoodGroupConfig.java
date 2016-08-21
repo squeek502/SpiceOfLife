072b3cf2dc8248873c59ff5bcc04368f0707b249
squeek <squeek502@hotmail.com>
2016-08-20 22:27:27 -0700

    Only read .json files when loading food groups  - Fixes #98

--- java/squeek/spiceoflife/foodtracker/foodgroups/FoodGroupConfig.java
+++ java/squeek/spiceoflife/foodtracker/foodgroups/FoodGroupConfig.java
@@ -83,6 +83,10 @@ public class FoodGroupConfig
 	{
 		for (File configFile : configFiles)
 		{
+			boolean isJson = FilenameUtils.getExtension(configFile.getName()).equalsIgnoreCase("json");
+			if (!isJson)
+				continue;
+
 			InputStreamReader reader = null;
 			try
 			{
